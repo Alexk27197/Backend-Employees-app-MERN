0c0993c76b8f294275c39e33abf561bf0031a4c6
Alex27197 <koganalexr@gmail.com>
2023-09-23 00:03:37 +0300

    add port to env

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use("/api/employees", employeesRouter);
-const PORT = 4010;
+const PORT = process.env.PORT || 4010;
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
